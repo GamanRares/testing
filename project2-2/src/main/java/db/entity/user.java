3974726490410d75328651671d935c8099f66e48
GamanRares <40998260+GamanRares@users.noreply.github.com>
2020-05-17 19:12:34 +0300

    Project2-3 Some field added.

--- project2-2/src/main/java/db/entity/user.java
+++ project2-2/src/main/java/db/entity/user.java
@@ -1,2 +1,12 @@
 public class User implements Serializable {
+  
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_id_generator")
+    @SequenceGenerator(name = "user_id_generator", sequenceName = "user_id_sequence")
+    private Long id;
+
+    @Size(min = 1, max = 100, message = "User email must be between 1 and 100 characters")
+    @Column(name = "email", unique = true, length = 100, columnDefinition = "VARBINARY(255)")
+    private String email;
+  
 }
